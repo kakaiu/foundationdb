5ecd775bf19211b6768fad8119eb2d4282b9be92
Zhe Wang <zhe.wang@wustl.edu>
2024-10-31 16:45:45 -0700

    address comments and rangeLock bypass lock-aware transactions

--- fdbserver/CommitProxyServer.actor.cpp
+++ fdbserver/CommitProxyServer.actor.cpp
@@ -738,9 +738,15 @@ struct CommitBatchContext {
 
 	void checkHotShards();
 
+	bool rangeLockEnabled();
+
 private:
 	void evaluateBatchSize();
 };
+
+bool CommitBatchContext::rangeLockEnabled() {
+	return pProxyCommitData->rangeLockEnabled();
+}
 
 void CommitBatchContext::checkHotShards() {
 	// removed expired hot shards
@@ -2010,16 +2016,15 @@ void addAccumulativeChecksumMutations(CommitBatchContext* self) {
 }
 // RangeLock takes effect only when the feature flag is on and database is unlocked and the mutation is not encrypted
 void rejectMutationsForReadLockOnRange(CommitBatchContext* self) {
-	ASSERT(SERVER_KNOBS->ENABLE_READ_LOCK_ON_RANGE && !SERVER_KNOBS->ENABLE_VERSION_VECTOR &&
-	       !SERVER_KNOBS->ENABLE_VERSION_VECTOR_TLOG_UNICAST && !SERVER_KNOBS->PROXY_USE_RESOLVER_PRIVATE_MUTATIONS &&
-	       !self->locked && !self->pProxyCommitData->encryptMode.isEncryptionEnabled() &&
-	       self->pProxyCommitData->getTenantMode() == TenantMode::DISABLED);
+	ASSERT(self->rangeLockEnabled());
 	ProxyCommitData* const pProxyCommitData = self->pProxyCommitData;
 	ASSERT(pProxyCommitData->rangeLock != nullptr);
 	std::vector<CommitTransactionRequest>& trs = self->trs;
 	for (int i = self->transactionNum; i < trs.size(); i++) {
 		if (self->committed[i] != ConflictBatch::TransactionCommitted) {
 			continue;
+		} else if (trs[i].isLockAware()) {
+			continue; // rangeLock is transparent to lock-aware transactions
 		}
 		VectorRef<MutationRef>* pMutations = &trs[i].transaction.mutations;
 		bool transactionRejected = false;
@@ -2344,11 +2349,7 @@ ACTOR Future<Void> postResolution(CommitBatchContext* self) {
 	// After applyed metadata change, this commit proxy has the latest view of locked ranges.
 	// If a transaction has any mutation accessing to the locked range, reject the transaction with
 	// error_code_transaction_rejected_range_locked
-	// This feature is disabled when the database is locked
-	if (SERVER_KNOBS->ENABLE_READ_LOCK_ON_RANGE && !SERVER_KNOBS->ENABLE_VERSION_VECTOR &&
-	    !SERVER_KNOBS->ENABLE_VERSION_VECTOR_TLOG_UNICAST && !SERVER_KNOBS->PROXY_USE_RESOLVER_PRIVATE_MUTATIONS &&
-	    !self->locked && !pProxyCommitData->encryptMode.isEncryptionEnabled() &&
-	    self->pProxyCommitData->getTenantMode() == TenantMode::DISABLED) {
+	if (self->rangeLockEnabled()) {
 		rejectMutationsForReadLockOnRange(self);
 	}
 
@@ -4010,6 +4011,12 @@ ACTOR Future<Void> commitProxyServerCore(CommitProxyInterface proxy,
 		                         SERVER_KNOBS->COMMIT_BATCHES_MEM_TO_TOTAL_MEM_SCALE_FACTOR));
 	}
 	TraceEvent(SevInfo, "CommitBatchesMemoryLimit").detail("BytesLimit", commitBatchesMemoryLimit);
+
+	// Initialize RangeLock
+	if (commitData.rangeLockEnabled()) {
+		commitData.rangeLock = std::make_shared<RangeLock>(&commitData);
+		TraceEvent(SevInfo, "CommitProxyRangeLockEnabled", commitData.dbgid);
+	}
 
 	addActor.send(monitorRemoteCommitted(&commitData));
 	addActor.send(tenantIdServer(proxy, addActor, &commitData));
